bba620013662d77b23243828b018a3b4d2b2228d
林浩南 <linhaonan@tiaozhan.com>
2019-01-17 00:07:27 +0800

    test

--- green.py
+++ green.py
@@ -21,7 +21,7 @@ def commit():
 
 
 def set_sys_time(year, month, day):
-    os.system('date -s %04d/%02d/%02d' % (year, month, day))
+    os.system('date -s %04d%02d%02d' % (year, month, day))
 
 
 def trick_commit(year, month, day):
@@ -37,4 +37,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2019, 1, 13), datetime.date(2020, 1, 15))
+    daily_commit(datetime.date(2019, 1, 13), datetime.date(2020, 1, 16))
